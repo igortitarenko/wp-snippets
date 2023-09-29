0458d64f5003ea6498b174671ff4f18079bcae60
John Doe <johndoe@example.com>
2023-09-29 13:58:35 +0400

    Final test version

--- restMpdf/assets/js/rest-mpdf.js
+++ restMpdf/assets/js/rest-mpdf.js
@@ -28,12 +28,10 @@
     /**
      * Fetches PDF with REST call.
      * Triggers PDF download.
-     * @param e
      */
-    function loadPDF(e) {
+    function loadPDF() {
         let data = {};
         data.nonce_local = apiSettings.nonce_local;
-        let link = document.createElement('a');
 
         postData(apiSettings.root + 'wpsnippets/v2/restmpdf', data)
             .then((restResponse) => {
@@ -43,7 +41,6 @@
                 a.href = "data:application/pdf;base64," + restResponse;
                 a.download = 'pdf-' + d + '.pdf';
 
-                console.log(a);
                 a.dispatchEvent(new MouseEvent('click'));
             });
     }
--- restMpdf/rest/RestMpdf.php
+++ restMpdf/rest/RestMpdf.php
@@ -4,100 +4,101 @@
  * Generates a PDF file.
  */
 
-namespace restMpdf\rest;
+namespace rest;
 
 use Mpdf\MpdfException;
+use Mpdf\Mpdf;
 
-if (!defined('ABSPATH')) {
-	exit; // Exit if accessed
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed
 }
 
 class RestMpdf
 {
     public function __construct()
     {
-		add_action('rest_api_init', $this->restRegister(...));
+        add_action('rest_api_init', $this->restRegister(...));
     }
 
-	/**
-	 * Registers the REST call.
-	 *
-	 * @return void
-	 */
-	public function restRegister(): void
-	{
-		/*
-		 * WordPress doesn't fail REST call if permission/sanitize/validate callbacks don't exist, so do it manually.
-		 */
-		$callbacks = [
-			'perm'      => 'permissionsCallback',
-			'valFil'    => 'validateLocalNonce',
-		];
+    /**
+     * Registers the REST call.
+     *
+     * @return void
+     */
+    public function restRegister(): void
+    {
+        /*
+         * WordPress doesn't fail REST call if permission/sanitize/validate callbacks don't exist, so do it manually.
+         */
+        $callbacks = [
+            'perm'   => 'permissionsCallback',
+            'valFil' => 'validateLocalNonce',
+        ];
 
-		foreach ($callbacks as $method) {
-			if (false === method_exists($this, $method)) {
-				wp_die('Callback doesn\'t exist');
-			}
-		}
+        foreach ($callbacks as $method) {
+            if (false === method_exists($this, $method)) {
+                wp_die('Callback doesn\'t exist');
+            }
+        }
 
-		register_rest_route(
-			'wpsnippets/v2',
-			'restmpdf',
-			[
-				'methods'  => 'POST',
-				'callback' => $this->restOutput(...),
-				'permission_callback' => $this->permissionsCallback(...),
-				'args' => [
-					'nonce_local' => [
-						'default' => false,
-						'validate_callback' => $this->validateLocalNonce(...),
-					],
-				],
-			]
-		);
-	}
+        register_rest_route(
+            'wpsnippets/v2',
+            'restmpdf',
+            [
+                'methods'             => 'POST',
+                'callback'            => $this->restOutput(...),
+                'permission_callback' => $this->permissionsCallback(...),
+                'args'                => [
+                    'nonce_local' => [
+                        'default'           => false,
+                        'validate_callback' => $this->validateLocalNonce(...),
+                    ],
+                ],
+            ]
+        );
+    }
 
-	/**
-	 * Rest call output.
-	 *
-	 * @return void
-	 * @throws MpdfException
-	 */
-	public function restOutput(): void
-	{
-		/* Headers */
-		header('Content-Description: File Transfer');
-		header('Content-Disposition: attachment; filename="data.pdf"');
-		header('Expires: 0');
-		header('Cache-Control: must-revalidate');
-		header('Pragma: public');
-		header("Content-Type: text/plain");
+    /**
+     * Rest call output.
+     *
+     * @return void
+     * @throws MpdfException
+     */
+    public function restOutput(): void
+    {
+        /* Headers */
+        header('Content-Description: File Transfer');
+        header('Content-Disposition: attachment; filename="data.pdf"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header("Content-Type: text/plain");
 
-		/* mPDF */
-		require_once(realpath(__DIR__ . '/../vendor/autoload.php'));
+        /* mPDF */
+        require_once(realpath(__DIR__ . '/../vendor/autoload.php'));
 
-		$mpdf = new \Mpdf\Mpdf([
-			'setAutoTopMargin'     => false,
-			'margin_bottom'        => 10,
-			'margin_left'          => 10,
-			'margin_right'         => 10,
-			'margin_top'           => 45,
-			'tableMinSizePriority' => true,
-		]);
+        $mpdf = new Mpdf([
+            'setAutoTopMargin'     => false,
+            'margin_bottom'        => 10,
+            'margin_left'          => 10,
+            'margin_right'         => 10,
+            'margin_top'           => 45,
+            'tableMinSizePriority' => true,
+        ]);
 
-		$mpdf->useFixedNormalLineHeight = false;
-		$mpdf->useFixedTextBaseline     = false;
-		$mpdf->adjustFontDescLineheight = 1.14;
-		$mpdf->use_kwt = true;
+        $mpdf->useFixedNormalLineHeight = false;
+        $mpdf->useFixedTextBaseline     = false;
+        $mpdf->adjustFontDescLineheight = 1.14;
+        $mpdf->use_kwt                  = true;
 
-		/* PDF-specific CSS */
-		$css = file_get_contents(realpath(__DIR__ . '/../assets/css/pdf.css'));
-		$mpdf->WriteHTML($css, \Mpdf\HTMLParserMode::HEADER_CSS);
+        /* PDF-specific CSS */
+        $css = file_get_contents(realpath(__DIR__ . '/../assets/css/pdf.css'));
+        $mpdf->WriteHTML($css, \Mpdf\HTMLParserMode::HEADER_CSS);
 
-		/* New page */
-		$mpdf->AddPageByArray([]);
+        /* New page */
+        $mpdf->AddPageByArray([]);
 
-		$header = '
+        $header = '
             <div class="header_container"><table class="header">
                 <tr>
                     <td class="header1">Header Title</td>
@@ -106,42 +107,43 @@ class RestMpdf
             </table></div>
         ';
 
-		$body = '<div><p>PDF Body</p></div>';
+        $body = '<div><p>PDF Body</p></div>';
 
-		$mpdf->SetHTMLHeader($header, 'BLANK', 'BLANK', true);
-		$mpdf->WriteHTML(stripslashes($body), \Mpdf\HTMLParserMode::HTML_BODY);
+        $mpdf->SetHTMLHeader($header, 'BLANK', 'BLANK', true);
+        $mpdf->WriteHTML(stripslashes($body), \Mpdf\HTMLParserMode::HTML_BODY);
 
-		echo base64_encode($mpdf->Output('data.file', 'S'));
-	}
+        echo base64_encode($mpdf->Output('data.file', 'S'));
+    }
 
-	/**
-	 * Checks for permissions.
-	 * In this case is empty.
-	 *
-	 * @return bool
-	 */
-	public function permissionsCallback(): bool
-	{
-		return true;
-	}
+    /**
+     * Checks for permissions.
+     * In this case is empty.
+     *
+     * @return bool
+     */
+    public function permissionsCallback(): bool
+    {
+        return true;
+    }
 
-	/**
-	 * Checks for a nonce.
-	 * This nonce is optional. Having this second nonce prevents public access to this REST call.
-	 *
-	 * @param string $nonce
-	 * @see $this->enqueueScript(...)
-	 *
-	 * @return bool
-	 */
-	public function validateLocalNonce(string $nonce): bool
-	{
-		if (false !== wp_verify_nonce($nonce, 'restMpdf')) {
-			return true;
-		}
+    /**
+     * Checks for a nonce.
+     * This nonce is optional. Having this second nonce prevents public access to this REST call.
+     *
+     * @param string $nonce
+     *
+     * @return bool
+     * @see $this->enqueueScript(...)
+     *
+     */
+    public function validateLocalNonce(string $nonce): bool
+    {
+        if (false !== wp_verify_nonce($nonce, 'restMpdf')) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
 
 new RestMpdf();
